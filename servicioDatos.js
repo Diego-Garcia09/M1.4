7095407144292ae768bb7398d5ebf561e8af5aa8
Diego-Garcia09 <112862634+Diego-Garcia09@users.noreply.github.com>
2023-08-31 07:47:24 -0700

    Implementación de callbacks

--- servicioDatos.js
+++ servicioDatos.js
@@ -1,8 +1,11 @@
-let finDeTransmisionDeDatos = function (id) {
-    console.log('Transferencia', id, 'terminada');
+let finDeTransmisionDeDatos = function(id, callback){
+    console.log('Transferencia',id,'terminada');
+    callback();
 }
-let obtenDatosDeInternet = function (id, duracion) {
-    console.log('Proceso', id, 'obteniendo datos de Internet') ;
-    setTimeout(finDeTransmisionDeDatos, duracion, id) ;
+
+let obtenDatosDeInternet = function(id, duracion, callback){
+    console.log('Proceso',id,'obteniendo datos de Internet');
+    setTimeout(finDeTransmisionDeDatos, duracion, id, callback);
 }
+
 module.exports.getDatos = obtenDatosDeInternet;
